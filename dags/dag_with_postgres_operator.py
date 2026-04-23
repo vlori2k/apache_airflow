906c520a2c4e54d6a00aa03bff3ff63fbdff2001
Vlorjan <vlorjanbadallaj@hotmail.com>
2026-04-23 12:44:41 +0200

    date : 15 april 2026 time : 09:50 - edging closer and closer. soon finished with the tutorial

---
+++ dags/dag_with_postgres_operator.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+default_args = {
+    'owner': 'Vlorjan',
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='dag_with_postgres_operator_v012',
+    default_args=default_args,
+    start_date=datetime(2026, 4, 20),
+    schedule='0 0 * * *',
+    catchup=False,
+) as dag:
+
+    task1 = SQLExecuteQueryOperator(
+        task_id='create_postgres_table',
+        conn_id='postgres_localhost',
+        sql="""
+        CREATE TABLE IF NOT EXISTS dag_runs (
+            dt DATE,
+            dag_id CHARACTER VARYING,
+            PRIMARY KEY (dt, dag_id)
+        )
+        """,
+    )
